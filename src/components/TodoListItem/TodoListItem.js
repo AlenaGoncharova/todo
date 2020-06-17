97113ce2283ae725f53bfd58c4f256fa412823b6
AlenaGoncharova <alen.gon4arowa@gmail.com>
2020-06-17 18:58:18 +0300

    Code refactoring

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -3,31 +3,8 @@ import React, { Component } from 'react';
 import './TodoListItem.css';
 
 export default class TodoListItem extends Component {
-
-  state = {
-    done: false,
-    important: false,
-  }
-
-  onLabelClick = () => {
-    this.setState(({ done }) => {
-      return {
-        done: !done,
-      };
-    });
-  };
-
-  onMarkImportant = () => {
-    this.setState(({ important}) => {
-      return {
-        important: !important
-      }
-    });
-  };
-
   render() {
-    const { label, onDeleted } = this.props;
-    const { done, important } = this.state;
+    const { done, important, label, onDeleted, onToggleDone, onToggleImportant } = this.props;
 
     let classNames = 'todo-list-item';
     if (done) {
@@ -41,13 +18,13 @@ export default class TodoListItem extends Component {
       <span className={classNames}>
         <span
           className="todo-list-item-label"
-          onClick={this.onLabelClick}>
+          onClick={onToggleDone}>
           {label}
         </span>
   
         <button type="button"
                 className="btn btn-outline-success btn-sm float-right"
-                onClick={this.onMarkImportant}>
+                onClick={onToggleImportant}>
           <i className="fa fa-exclamation" />
         </button>
   
