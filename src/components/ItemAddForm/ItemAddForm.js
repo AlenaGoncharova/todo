d78b829f332d0538269167b69f1cca671906ddf1
AlenaGoncharova <alen.gon4arowa@gmail.com>
2020-06-17 20:15:23 +0300

    Add functionality to add list items

---
+++ src/components/ItemAddForm/ItemAddForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+import './ItemAddForm.css';
+
+export default class ItemAddForm extends Component {
+    
+    state = {
+        label: '',
+    }
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        const { label } = this.state;
+        if (label !== '') {
+            this.props.onItemAdded(label);
+            this.setState({
+                label: '',
+            });
+        }
+    }
+
+    render() {
+        return (
+            <form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+                <input type="text"
+                    className="form-control"
+                    onChange={this.onLabelChange}
+                    placeholder="What needs to be done"
+                    value={this.state.label}
+                />
+                <button className="btn btn-outline-secondary">Add</button>
+            </form>
+        )
+    }
+}
